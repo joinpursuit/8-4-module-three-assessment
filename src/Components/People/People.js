98ff25a016a1c7f67a96bb5ce6645a1895c434ae
John Prado <jcprado20@icloud.com>
2022-06-24 17:17:59 -0400

    adds foundPerson but not working

--- src/Components/People/People.js
+++ src/Components/People/People.js
@@ -1,14 +1,37 @@
-import React from 'react'
-import "./People.css"
+import React, { useEffect, useState } from "react";
+import "./People.css";
+import PersonCard from "./PersonCard";
 
 function People() {
+
+  const [people, setPeople] = useState([]);
+  const [input, setInput] = useState("");
+  // console.log(people);
+
+  useEffect(() => {
+    fetch("https://ghibliapi.herokuapp.com/people")
+      .then((response) => response.json())
+      .then((data) => setPeople(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const foundPerson = people.find(person => person.name === input)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // setInput(e.target.value)
+  }
+
+  console.log(foundPerson)
   return (
     <div>
       <h1>Search for a Person</h1>
-      <input type="text"></input>
-      <button>Submit</button>
+      <form onSubmit={handleSubmit}>
+        <input onChange={(e) => setInput(e.target.value)}type="text" ></input>
+        <button>Submit</button>
+      </form>
+      <PersonCard foundPerson={foundPerson}/>
     </div>
-  )
+  );
 }
 
-export default People
+export default People;
--- src/Components/People/PersonCard.js
+++ src/Components/People/PersonCard.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
-function PersonCard() {
+
+
+function PersonCard({ foundPerson }) {
+
+  if(!foundPerson) {
+    return(
+      <div>
+        <p>Please enter a person's name!</p>
+      </div>
+    )
+  }  
   return (
     <div>
+      <h1>Name: {foundPerson.name}</h1>
+      <p><strong>Age: </strong>{foundPerson.age}</p>
+      <p><strong>Eye Color: </strong>{foundPerson.eye_color}</p>
+      <p><strong>Hair Color: </strong>{foundPerson.hair_color}</p>
       
     </div>
   )
