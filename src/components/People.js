132f2cac7e45f17cdda53183aee154cf00405797
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2022-06-24 15:47:04 -0400

    adss people page

--- src/components/People.js
+++ src/components/People.js
@@ -1,9 +1,40 @@
-const People = () =>{
-    return (
-        <div className="people">
-            <p>People</p>
-        </div>
-    )
-}
+import { useEffect, useState } from "react";
+import Person from "./Person";
+
+const People = () => {
+  const [userRequest, setUserRequest] = useState("");
+  const [people, setPeople] = useState([]);
+  const [person, setPerson] = useState();
+
+  useEffect(() => {
+    fetch("https://ghibliapi.herokuapp.com/people")
+      .then((res) => res.json())
+      .then((data) => setPeople(data));
+  }, []);
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+
+    const foundPerson = people.find(person => person.name.toLowerCase() == userRequest.toLowerCase())
+
+    setPerson(foundPerson)
+    setUserRequest("")
+  }
+
+  return (
+    <div className="people">
+      <h1>Search for a Person</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={userRequest}
+          onChange={(e) => setUserRequest(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {person ? <Person person={person} /> : null}
+    </div>
+  );
+};
 
 export default People;
---
+++ src/components/Person.js
@@ -0,0 +1,12 @@
+const Person = ({person}) => {
+    return (
+        <div className="person">
+            <h1>Name: {person.name}</h1>
+            <p><span>Age: </span>{person.age}</p>
+            <p><span>Eye Color: </span>{person.eye_color}</p>
+            <p><span>Hair Color: </span>{person.hair_color}</p>
+        </div>
+    )
+}
+
+export default Person;
