0715e812350c55dcf1502540bb79754051c8c037
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-06-25 17:08:56 -0400

    "Movies"

---
+++ src/Components/MovieCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function MovieCard({ option, movies }) {
+  const selectedMovie = movies.find((movie) => movie.id === option)
+   
+    if(!option){
+      return (
+        <div>
+          <p>Please make a movie selection</p>
+        </div>
+      )
+    }
+  return (
+    <div>
+      <h1>Title: {selectedMovie.title}</h1>
+      <p><strong>Release Date: </strong>{selectedMovie.release_date}</p>
+      <p><strong>Description: </strong>{selectedMovie.description}</p>
+    </div>
+  )
+}
+
+export default MovieCard
--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,21 +1,36 @@
-import React, { useEffect, useState } from "react";
-import "./Movies.css"
+import React, { useState, useEffect } from "react";
+import "./Movies.css";
+import MovieCard from "./MovieCard";
 
 function Movies() {
-
+  const [movies, setMovies] = useState([]);
+  const [option, setOption] = useState("");
   
 
+  useEffect(() => {
+    fetch(`https://ghibliapi.herokuapp.com/films`)
+      .then((response) => response.json())
+      .then((data) => setMovies(data))
+      .catch((err) => console.log(err));
+  }, []);
 
-
+  const handleChange = (e) => {
+    e.preventDefault();
+    setOption(e.target.value);
+  };
 
   return (
-    <div className="center">
-      <header className="movies">
-        <h1>Select a Movie</h1>
-        <select className="select">
+    <div className="movies">
+      <h1>Select a Movie</h1>
+      <select onChange={handleChange}>
         <option></option>
-        </select>
-      </header>
+        {movies.map((movie) => (
+          <option key={movie.id} value={movie.id}>
+            {movie.title}
+          </option>
+        ))}
+      </select>
+      <MovieCard movies={movies} option={option} />
     </div>
   );
 }
