3d64e23435718637a9c0774755146b21bafb5475
T.K <nique@niques-air.myfiosgateway.com>
2022-06-26 16:50:23 -0400

    trying

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,54 +3,46 @@ import { useEffect } from "react";
 import "./Movies.css";
 
 export default function Movies({ movie, setMovie }) {
-
-    // const [click, setClick] = useState(false)
-
   useEffect(() => {
     fetch("https://ghibliapi.herokuapp.com/films")
       .then((response) => response.json())
       .then((films) => {
         // console.log(films)
-        setMovie(films)
+        setMovie(films);
       })
       .catch((error) => {
-        console.log(error)
-      })
+        console.log(error);
+      });
   }, [setMovie]);
 
   const handleChange = (e) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+  };
 
-//   const handleClick = (e) => {
-//     e.preventDefault()
-//     click ? setClick(false) : setClick(true)
-//   }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    movie.map((mov) => {
+      return (
+        <div>
+          <h1>Title: {mov.title}</h1>
+          <h3>Release Date: {mov.release_date}</h3>
+          <h3>Description: {mov.description}</h3>
+        </div>
+      );
+    });
+  };
 
   return (
     <div className="movies">
       <h1>Select a Movie</h1>
-      <form>
-        <select onChange={ handleChange }>
-            <option></option>
-            {movie.map((mov) => {
-                return (
-                    <option>{mov.title}</option>
-                )
-            })}
+      <form onSubmit={handleSubmit}>
+        <select onChange={handleChange}>
+          <option></option>
+          {movie.map((mov) => {
+            return <option>{mov.title}</option>;
+          })}
         </select>
       </form>
     </div>
   );
 }
-
-
-//  {/* {movie.map((mov) => {
-//             return (
-//                 <div>
-//                 <h1>Title: {mov.title}</h1>
-//                 <h3>Release Date: {mov.release_date}</h3>
-//                 <h3>Description: {mov.description}</h3>
-//                 </div>
-//             );
-//         })} */}
