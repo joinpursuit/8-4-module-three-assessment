74e77edc4b7aa1fcbc5f4934f8d7f0262bc4b363
KenyaMighty <kenyamighty@pursuit.org>
2022-06-26 12:01:31 -0400

    add three components

---
+++ src/Components/Locations.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Locations extends Component {
+  render() {
+    return (
+        <div>
+          <h2>Locations</h2>
+        </div>
+    );
+  }
+}
+
+export default Locations;
