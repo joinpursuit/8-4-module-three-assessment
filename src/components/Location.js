e3c54d3cf33c9f79ab8cac493d2cdc059a96559a
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2022-06-26 19:05:29 -0400

    done

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,47 +1,45 @@
-import "./Location.css"
+import "./Location.css";
 
-const Location = ({location, sortName, sortClimate, sortTerrain}) =>{
-    if(sortName){
-        return (
-            <div className="location">
-                <ul>
-                    <li>Climate: <span>{location.climate}</span></li>
-                    <li>Name: <span>{location.name}</span></li>
-                    <li>Terrain: <span>{location.terrain}</span></li>
-                </ul>
-            </div>
-        )
-    } else if(sortClimate){
-        return (
-            <div className="location">
-                <ul>
-                    <li>Climate: <span>{location.climate}</span></li>
-                    <li>Terrain: <span>{location.terrain}</span></li>
-                    <li>Name: <span>{location.name}</span></li>
-                </ul>
-            </div>
-        )
-    }else if(sortTerrain){
-        return (
-            <div className="location">
-                <ul>
-                    <li>Terrain: <span>{location.terrain}</span></li>
-                    <li>Climate: <span>{location.climate}</span></li>
-                    <li>Name: <span>{location.name}</span></li>
-                </ul>
-            </div>
-        )
-    }else{
-        return (
-            <div className="location">
-                <ul>
-                    <li>Name: <span>{location.name}</span></li>
-                    <li>Climate: <span>{location.climate}</span></li>
-                    <li>Terrain: <span>{location.terrain}</span></li>
-                </ul>
-            </div>
-        )
-    }
-}
+const Location = ({ location, sort }) => {
+  if (sort) {
+    return (
+      <div className="location">
+        <ul>
+          <li>
+            <span className="span-one">Name: </span>
+            <span className="span-two">{location.name}</span>
+          </li>
+          <li>
+            <span className="span-one">Climate: </span>
+            <span className="span-two">{location.climate}</span>
+          </li>
+          <li>
+            <span className="span-one">Terrain: </span>
+            <span className="span-two">{location.terrain}</span>
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div className="location">
+        <ul>
+          <li>
+            <span className="span-one">Name: </span>
+            <span className="span-two">{location.name}</span>
+          </li>
+          <li>
+            <span className="span-one">Climate: </span>
+            <span className="span-two">{location.climate}</span>
+          </li>
+          <li>
+            <span className="span-one">Terrain: </span>
+            <span className="span-two">{location.terrain}</span>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+};
 
 export default Location;
--- src/components/Locations.js
+++ src/components/Locations.js
@@ -1,102 +1,103 @@
 import { useState, useEffect } from "react";
 import Location from "./Location";
-import "./Locations.css"
+import "./Locations.css";
 
 const Locations = () => {
-    const [visible, setVisible] = useState(false);
-    const [locations, setLocations] = useState([]);
-    const [sortName, setSortName] = useState(false);
-    const [sortClimate, setSortClimate] = useState(false);
-    const [sortTerrain, setSortTerrain] = useState(false);
+  const [visible, setVisible] = useState(false);
+  const [locations, setLocations] = useState([]);
+  const [sort, setSort] = useState(false);
 
-    useEffect(() =>{
-        fetch("https://ghibliapi.herokuapp.com/locations")
-        .then(res => res.json())
-        .then(data => setLocations(data))
-    }, [])
+  useEffect(() => {
+    fetch("https://ghibliapi.herokuapp.com/locations")
+      .then((res) => res.json())
+      .then((data) => setLocations(data));
+  }, []);
 
-    const locationsCopied = [...locations];
+  const locationsCopied = [...locations];
 
-    const section = () =>{
-        if(visible){
-            if(sortName){
-                return locationsCopied.sort((a, b) => {
-                    if(a.name.toLowerCase() < b.name.toLowerCase()){
-                        return -1
-                    }
-                    if(a.name.toLowerCase() > b.name.toLowerCase()){
-                        return 1
-                    }
-                    return 0
-                }).map(location =>{
-                    return (
-                        <ul>
-                            <li><Location location={location} sortName={sortName} sortClimate={sortClimate} sortTerrain={sortTerrain}/></li>
-                        </ul>
-                    )
-                })
-            };
-            
-            if(sortClimate){
-                return locationsCopied.sort((a, b) => {
-                    if(a.climate.toLowerCase() < b.climate.toLowerCase()){
-                        return -1
-                    }
-                    if(a.climate.toLowerCase() > b.climate.toLowerCase()){
-                        return 1
-                    }
-                    return 0
-                }).map(location =>{
-                    return (
-                        <ul>
-                            <li><Location location={location} sortName={sortName} sortClimate={sortClimate} sortTerrain={sortTerrain}/></li>
-                        </ul>
-                    )
-                })
-            };
-            
-            if(sortTerrain){
-                return locationsCopied.sort((a, b) => {
-                    if(a.terrain.toLowerCase() < b.terrain.toLowerCase()){
-                        return -1
-                    }
-                    if(a.terrain.toLowerCase() > b.terrain.toLowerCase()){
-                        return 1
-                    }
-                    return 0
-                }).map(location =>{
-                    return (
-                        <ul>
-                            <li><Location location={location} sortName={sortName} sortClimate={sortClimate} sortTerrain={sortTerrain}/></li>
-                        </ul>
-                    )
-                })
-            };
+  const allLocations = () => {
+    return locationsCopied.map((location) => {
+      return (
+        <ul>
+          <li>
+            <Location location={location} sort={sort} />
+          </li>
+        </ul>
+      );
+    });
+  };
 
-            return locations.map(location => {
-                return (
-                    <ul>
-                        <li><Location location={location} sortName={sortName} sortClimate={sortClimate} sortTerrain={sortTerrain}/></li>
-                    </ul>
-                )
-            })
-        }else{
-            return null;
-        };
-    }
+  const handleNameSort = () => {
+    setLocations(
+        locationsCopied.sort((a, b) => {
+        if (a.name.toLowerCase() < b.name.toLowerCase()) {
+          return -1;
+        }
+        if (a.name.toLowerCase() > b.name.toLowerCase()) {
+          return 1;
+        }
+        return 0;
+      })
+    );
+    setSort(true);
+  };
 
-    return (
-        <div className="locations">
-            <h1>List of Locations</h1>
-            <section className="buttons">
-            <button type="submit" className="show" onClick={() => !visible ? setVisible(true) : setVisible(false)}> {!visible ? "Show Locations" : "Hide Locations"}</button>
-            {visible ? <button type="submit" className="name" onClick={() => !sortName ? setSortName(true) : setSortName(false)}>Sort by Name</button> : null}
-            {visible ? <button type="submit" className="climate" onClick={() => !sortClimate ? setSortClimate(true) : setSortClimate(false)}>Sort by Climate</button> : null}
-            {visible ? <button type="submit" className="terrain" onClick={() => !sortTerrain ? setSortTerrain(true) : setSortTerrain(false)}>Sort by Terrain</button> : null}
-            </section>
-            {section()}
-        </div>
-    )
-}
+  const handleClimateSort = () => {
+    setLocations(
+        locationsCopied.sort((a, b) => {
+        if (a.climate.toLowerCase() < b.climate.toLowerCase()) {
+          return -1;
+        }
+        if (a.climate.toLowerCase() > b.climate.toLowerCase()) {
+          return 1;
+        }
+        return 0;
+      })
+    );
+    setSort(true)
+  };
+
+  const handleTerrainSort = () =>{
+    setLocations(
+        locationsCopied.sort((a, b) => {
+          if (a.terrain.toLowerCase() < b.terrain.toLowerCase()) {
+            return -1;
+          }
+          if (a.terrain.toLowerCase() > b.terrain.toLowerCase()) {
+            return 1;
+          }
+          return 0;
+        })
+      );
+      setSort(true)
+  }
+
+  return (
+    <div className="locations">
+      <h1>List of Locations</h1>
+      <section className="buttons">
+        <button
+          type="submit"
+          className="show"
+          onClick={() => (!visible ? setVisible(true) : setVisible(false))}
+        >
+          {!visible ? "Show Locations" : "Hide Locations"}
+        </button>
+
+        {visible ? (
+          <button type="submit" className="name" onClick={handleNameSort}>
+            Sort by Name
+          </button>
+        ) : null}
+
+        {visible ? <button type="submit" className="climate" onClick={handleClimateSort}>Sort by Climate</button> : null}
+
+        {visible ? <button type="submit" className="terrain" onClick={handleTerrainSort}>Sort by Terrain</button> : null}
+      </section>
+
+      {visible ? allLocations() : null}
+    </div>
+  );
+};
 
 export default Locations;
