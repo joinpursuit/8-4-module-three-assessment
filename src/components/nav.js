74f843520f27ca0189f538f6651608a6c8367f80
Tom LaTulipe <tomlatulipe@pursuit.org>
2022-06-26 01:28:12 -0400

    home image

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Nav from "react-bootstrap/Nav"
+import Nav from "react-bootstrap/Nav";
+import catbus from "../0014_catbus_mat_grande.webp";
+
 
 export const Navbar = () => {
+
   return (
     <nav>
       <Nav justify variant="tabs" className="Nav" className="justify-content-center" activeKey={"/"}>
           <Nav.Item>
-            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/"><img src={catbus} width="100px" id="catbus" alt="catbus!"></img></Nav.Link>
           </Nav.Item>
           <Nav.Item>
             <Nav.Link href="/locations">Locations</Nav.Link>
