3ed6d4d72608efc44eec456107f7a38861b5d4ae
KenyaMighty <kenyamighty@pursuit.org>
2022-06-26 13:42:56 -0400

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Home from "./Components/Home";
-import Movies from "./Components/Movies";
-import People from "./Components/People";
-import Locations from "./Components/Locations";
+import Navbar from "./Components/Navbar";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-      <div>
-        <h2>Welcome to GhibliAppl</h2>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <ul className="navbar-nav mr-auto">
-          <li><Link to={'/'} className="nav-link"> Home </Link></li>
-          <li><Link to={'/movies'} className="nav-link">Contact</Link></li>
-          <li><Link to={'/about'} className="nav-link">About</Link></li>
-        </ul>
-        </nav>
-        <hr />
-        <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/Movies' component={Movies} />
-            <Route path='/People' component={People} />
-            <Route path='/Locations' component={Locations} />
-        </Switch>
-      </div>
-    </Router>
-    );
-  }
-  }
-  
+function App() {
+  return (
+    <div className="App">
+    <Navbar/>
+    </div>
+  );
+}
 
 export default App;
