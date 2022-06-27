11610552199fbfb02131c97b8c8f34b68b4cdba2
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-06-26 20:02:19 -0400

    "almost"

--- src/Components/People.js
+++ src/Components/People.js
@@ -4,14 +4,12 @@ import "./People.css";
 import PeopleCard from "./PeopleCard";
 
 function People() {
-
-// create a useState for list of people set to empty array 
-// create a useState for user input set to an empty string - input element
+  // create a useState for list of people set to empty array
+  // create a useState for user input set to an empty string - input element
   const [people, setPeople] = useState([]);
   const [input, setInput] = useState("");
 
-
-    // fetch people data from ghibliapi and set returned data to updated useState people list - setPeople - w/ catch for any 404 errors
+  // fetch people data from ghibliapi and set returned data to updated useState people list - setPeople - w/ catch for any 404 errors
   useEffect(() => {
     fetch("https://ghibliapi.herokuapp.com/people")
       .then((response) => response.json())
@@ -19,30 +17,29 @@ function People() {
       .catch((error) => console.log(error));
   }, []);
 
-  
-  const foundPerson = people.find(person => person.name === input)
+  // create a variable for the person the user searched in the input element
+  const foundPerson = people.find((person) => person.name === input);
 
-    // create EVENT on handleSubmit that wont refresh the page  - when updated useState for user input value changes - setInput 
+  // create EVENT on handleSubmit that wont refresh the page  - when updated useState for user input value changes - setInput
   const handleSubmit = (e) => {
     e.preventDefault();
-    setInput(e.target.value)
-  }
+    setInput(e.target.value);
+  };
 
-  console.log(foundPerson)
+  console.log(foundPerson);
   return (
     <div>
       <h1>Search for a Person</h1>
-            {/* handleSumbit EVENT will run on - form - onSubmit */}
+      {/* handleSumbit EVENT will run on - form - onSubmit */}
       <form onSubmit={handleSubmit}>
-              {/* depending on the user input update the useState with new value  */}
-        <input onChange={(e) => setInput(e.target.value)} type="text" ></input> 
-        
+        {/* depending on the user input update the useState with new value  */}
+        <input onChange={(e) => setInput(e.target.value)} type="text"></input>
         <button>Submit</button>
       </form>
-      <PeopleCard foundPerson={foundPerson}/>
+      {/* transfer that person searched by the user to the PeopleCard Component */}
+      <PeopleCard foundPerson={foundPerson} />
     </div>
   );
 }
 
 export default People;
-
--- src/Components/PeopleCard.js
+++ src/Components/PeopleCard.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 
-
+// add the searched person from the People Component as a prop to the PeopleCard Component
 function PeopleCard({ foundPerson }) {
 
+  // if there is no person searched will return a message "Please enter a person's name"
   if(!foundPerson) {
     return(
       <div>
@@ -11,6 +12,7 @@ function PeopleCard({ foundPerson }) {
       </div>
     )
   }  
+  // else it will return the person's name, age, eye color and hair color
   return (
     <div>
       <h1>Name: {foundPerson.name}</h1>
