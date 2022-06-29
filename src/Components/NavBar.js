6596f9c6b9ba94d978a287b444d9c5af901e743d
King Ankoue <thierryankoue@pursuit.com>
2022-06-28 20:29:30 -0400

    final

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../logo.svg";
+
 
 const Nav = () => {
   return (
     <nav>
       <div className='Navbar'>
-        <Link to='/'>
-          <img src={logo} alt='GhibliApp logo' className='logo' />
-        </Link>
         <ul className='nav-links'>
           <li>
             <Link to='/Movies'>Movies</Link>
