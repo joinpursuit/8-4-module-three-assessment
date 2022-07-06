b6684fd8c684ebdf39f79ea22d03665d1926e841
T.K <nique@niques-air.myfiosgateway.com>
2022-07-06 17:51:13 -0400

    fixing people

--- src/components/People.js
+++ src/components/People.js
@@ -5,6 +5,7 @@ import "./People.css";
 export default function People() {
 
   const [people, setPeople] = useState([]);
+  const [input, setInput] = useState('');
 
   useEffect(() => {
         fetch("https://ghibliapi.herokuapp.com/people")
@@ -17,16 +18,27 @@ export default function People() {
           })
       }, [setPeople]);
 
-      // const handleChange = (e) => {
-      //   setPeople(e.target.value)
-      // }
+      const handleChange = (e) => {
+        const name = e.target.value
+        const person = people.find((peeps) => peeps.name === name) || {};
+        setInput(person)
+      }
+
+      const personDescription = input.name ? (
+        <div>
+          <h2>Name: { input.name }</h2>
+          <h3>Age: { input.age }</h3>
+          <h3>Gender: { input.gender }</h3>
+        </div>
+      ) : null;
 
   return (
     <div className='people'>
       <h1>Search for a Person</h1>
       <form>
-        <input value={ people }/>
+        <input onChange={ handleChange }/>
         <button>Submit</button>
+        <section>{ personDescription }</section>
       </form>
     </div>
   )
